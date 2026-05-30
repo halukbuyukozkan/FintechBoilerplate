cee2fbe5810862413dff8ce7977841c253c4f17b
halukbuyukozkan <halukbuyukozkan@gmail.com>
2026-05-30 14:58:41 +0300

    Add DashboardEurekaServerApp with dependencies and configuration

---
+++ DashboardEurekaServer/src/main/java/com/buyukozkan/eureka/DashboardEurekaServerApp.java
@@ -0,0 +1,15 @@
+package com.buyukozkan.eureka;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class DashboardEurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardEurekaServerApp.class, args);
+    }
+}
